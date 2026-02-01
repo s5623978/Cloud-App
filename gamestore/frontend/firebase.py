8d9bf0b232720df11f2d132af55c02d29279a042
Jess Booth <s5623978@bournemouth.ac.uk>
2026-02-01 18:53:17 +0100

    Collect static files

--- gamestore/frontend/firebase.py
+++ gamestore/frontend/firebase.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate("firebase-key.json")
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred)
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
